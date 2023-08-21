90dbe46d3ef62058ded616f9cb17d107b7e1fe0f
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-21 02:05:04 -0300

    feat: :wrench: Configuração inicial do mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env"
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URL)
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
